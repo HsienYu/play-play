8daddf76f6779ed0cb03fdd953600752e178dca2
chenghsienyu <chenghsienyu@aim.com>
2022-10-19 21:07:57 +0800

    updated and remove some files

--- rpdeneme.py
+++ rpdeneme.py
@@ -1,25 +1,23 @@
 from selenium import webdriver
 import time
 import pickle
-from movielist import liste
+from movielist import script_list
 import requests
 
-#print(liste)
+# print(script_list)
 driver = webdriver.Chrome()
 
-for i in range(len(liste)):
-    path = "https://imsdb.com/scripts/{}.html".format(liste[i])
+for i in range(len(script_list)):
+    path = "https://imsdb.com/scripts/{}.html".format(script_list[i])
     req = requests.get(path)
     if req.status_code == requests.codes['ok']:
-        print(liste[i])
+        print(script_list[i])
         driver.get(path)
         path1 = "/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr/td/pre"
-        dos = open("scripts.txt","a")
+        dos = open("scripts.txt", "a")
         time.sleep(1)
         try:
             dos.write(driver.find_element_by_xpath(path1).text)
-            print(liste[i],"completed")
+            print(script_list[i], "completed")
         except UnicodeEncodeError:
             pass
-
-
